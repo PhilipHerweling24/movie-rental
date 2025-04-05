8201f7fad9b145fbf8c4532b654e4dabf7fe7322
PhilipHerweling <miggymorette@gmail.com>
2025-04-05 11:36:37 +0100

    Initialising Micro Service

---
+++ src/main/java/com/a00326153/rental/MovieRentalApplication.java
@@ -0,0 +1,13 @@
+package com.a00326153.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a00326153/rental/MovieRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a00326153.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
