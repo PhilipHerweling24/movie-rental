308b038529adb353509558d5c5d652ddd0ddfd72
PhilipHerweling <miggymorette@gmail.com>
2025-04-06 14:09:46 +0100

    added dependency and annotation

--- src/main/java/com/a00326153/rental/MovieRentalApplication.java
+++ src/main/java/com/a00326153/rental/MovieRentalApplication.java
@@ -2,9 +2,11 @@ package com.a00326153.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieRentalApplication {
 
 	public static void main(String[] args) {
